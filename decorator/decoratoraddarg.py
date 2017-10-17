fcf363e12a0059c78e488257a33f33e70d8513b6
chainly <1258626769@qq.com>
2017-10-17 12:47:14 +0800

    Update decoratoraddarg.py
    for `inspect.signature`

--- decorator/decoratoraddarg.py
+++ decorator/decoratoraddarg.py
@@ -2,13 +2,24 @@
 # should not change orignal function!!!
 # http://chimera.labs.oreilly.com/books/1230000000393/ch09.html#decoratoraddarg
 from functools import wraps
+import inspect
 
 def optional_debug(func):
+    if 'debug' in inspect.getargspec(func).args:
+        raise TypeError('debug argument already defined')
+
     @wraps(func)
     def wrapper(*args, debug=False, **kwargs):
         if debug:
             print('Calling', func.__name__)
         return func(*args, **kwargs)
+
+    sig = inspect.signature(func)
+    parms = list(sig.parameters.values())
+    parms.append(inspect.Parameter('debug',
+                                   inspect.Parameter.KEYWORD_ONLY,
+                                   default=False))
+    wrapper.__signature__ = sig.replace(parameters=parms)
     return wrapper
     
 """
