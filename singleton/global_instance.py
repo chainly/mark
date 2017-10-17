a78975d3356a257d69a5f31e1ac05d94526e6a96
chainly <1258626769@qq.com>
2017-10-17 18:23:45 +0800

    Create global_instance.py

---
+++ singleton/global_instance.py
@@ -0,0 +1,14 @@
+# http://chimera.labs.oreilly.com/books/1230000000393/ch09.html#_discussion_156
+# 
+class _Spam:
+    def __init__(self):
+        print('Creating Spam')
+
+_spam_instance = None
+def Spam():
+    global _spam_instance
+    if _spam_instance is not None:
+        return _spam_instance
+    else:
+        _spam_instance = _Spam()
+        return _spam_instance
