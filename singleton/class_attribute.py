b3489a15182f69f7bdd70f3c7154ee055b140c82
chainly <1258626769@qq.com>
2019-02-24 21:30:49 +0800

    Update class_attribute.py

--- singleton/class_attribute.py
+++ singleton/class_attribute.py
@@ -1,2 +1,15 @@
-# like `from testfixtures import LogCapture`
-# use class's class_attribute
+# like `from testfixtures import LogCapture` # No: this is for log instances
+# use class's class_attribute; basely same as metaclass.attr
+class A():
+  
+    _instance = None  # not set()
+  
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+         
+        return cls._instance
+
+a = A()
+b = A()
+print(a, b)
